920616378a292a8cfdcdc2c447005478c036e1ca
wear工程师 <1225186748@qq.com>
2023-06-12 13:47:16 +0800

    chore: 更新文档,新增ci

---
+++ hack/releaser.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 检查GOOS GOARCH 是否设置
+if [ -z "$GOOS" ]; then
+  export GOOS=$(go env GOOS)
+fi
+
+if [ -z "$GOARCH" ]; then
+  export GOARCH=$(go env GOARCH)
+fi
+
+swag init -d .. -o ../docs
+
+# 打包
+rm -rf ./out/*
+rm -rf ./temp/*
+
+go build -o ./temp/library_sys_go-${GOOS}-${GOARCH} -trimpath -ldflags="-s -w" ../main.go
+
+cp -r ../docs ./temp/docs
+
+tar -zcvf ./out/library_sys_go-${GOOS}-${GOARCH}.tar.gz -C ./temp/ .
